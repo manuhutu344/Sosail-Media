0686ca7eb6adc9c91c5646920b6da3bd8d3c3259
manuhutu344 <nandoperdos2@gmail.com>
2023-01-16 23:35:50 +0900

    membuat icon foto di frontend dapat di klik

--- src/Component/ContentPostContainer/ContentPost.js
+++ src/Component/ContentPostContainer/ContentPost.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Profil from '../assets/Profil.jpg'
 import imageIcon from '../assets/gallery.png'
 import emoji from '../assets/cat-face.png'
@@ -11,6 +11,7 @@ function ContentPost() {
   let user = userDetails.user
   console.log(user)
   let id = user.other._id
+  const [file, setfile] = useState(null)
   return (
     <div>
     <div className='contentUploadContainer'>
@@ -20,7 +21,10 @@ function ContentPost() {
     </div>
     <div style={{display: 'flex', marginLeft: '5px'}}>
     <div>
+    <label htmlFor='file'>
     <img src={imageIcon} className='icons' alt='' />
+    <input type='file' name='file' id='file' style={{display:'none'}} />
+    </label>
     <img src={emoji} className='icons' alt='' />
     <img src={Iconvideo} className='icons' alt='' />
     <button style={{height:"27px" ,marginRight:"12px",marginTop:"40px", paddingLeft:"21px" , paddingRight:"21px" , paddingTop:7 , paddingBottom:7 , border:"none" , backgroundColor:"black" , color:"white" , borderRadius:"5px" , cursor:"pointer", marginLeft:'280px', marginBottom:'12px'}}>Posting</button>
