3582bc60b614311d9d38d6d2c7259d43d874fff7
manuhutu344 <nandoperdos2@gmail.com>
2023-01-17 10:23:26 +0900

    membuat pengupload ke firebase

--- src/Component/ContentPostContainer/ContentPost.js
+++ src/Component/ContentPostContainer/ContentPost.js
@@ -6,6 +6,7 @@ import Iconvideo from '../assets/video.png'
 import './ContentPost.css'
 import { useSelector } from 'react-redux'
 import app from '../../firebase'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 function ContentPost() {
   const userDetails = useSelector((state)=>state.user)
@@ -13,7 +14,41 @@ function ContentPost() {
   console.log(user)
   let id = user.other._id
   const [file, setfile] = useState('')
-  console.log(file.name)
+  const [file2, setfile2] = useState('')
+  console.log(file?.name)
+  const handlePost = (e)=>{
+    e.preventDefault()
+    const fileName = new Date().getTime() + file?.name
+    const storage = getStorage(app)
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+    }
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+    });
+  }
+);
+  }
   return (
     <div>
     <div className='contentUploadContainer'>
@@ -30,9 +65,9 @@ function ContentPost() {
     <img src={emoji} className='icons' alt='' />
     <label htmlFor='file2'>
     <img src={Iconvideo} className='icons' alt='' />
-    <input type='file' name='file2' id='file2' style={{display:'none'}} />
+    <input type='file' name='file2' id='file2' style={{display:'none'}} onChange={(e)=>setfile2(e.target.files[0])}/>
     </label>
-    <button style={{height:"27px" ,marginRight:"12px",marginTop:"40px", paddingLeft:"21px" , paddingRight:"21px" , paddingTop:7 , paddingBottom:7 , border:"none" , backgroundColor:"black" , color:"white" , borderRadius:"5px" , cursor:"pointer", marginLeft:'280px', marginBottom:'12px'}}>Posting</button>
+    <button style={{height:"27px" ,marginRight:"12px",marginTop:"40px", paddingLeft:"21px" , paddingRight:"21px" , paddingTop:7 , paddingBottom:7 , border:"none" , backgroundColor:"black" , color:"white" , borderRadius:"5px" , cursor:"pointer", marginLeft:'280px', marginBottom:'12px'}}onClick={handlePost}>Posting</button>
     </div>
     </div>
     </div>
