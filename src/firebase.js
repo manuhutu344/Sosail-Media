b6db377f2669c1fbb5f9d6ee529dd067c517a06a
manuhutu344 <nandoperdos2@gmail.com>
2023-01-17 09:28:51 +0900

    Menambahkan file firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUsE6lAReda4Eu-FuvAJ0fF9cv0fDzIG0",
+  authDomain: "sosialmedia-ae893.firebaseapp.com",
+  projectId: "sosialmedia-ae893",
+  storageBucket: "sosialmedia-ae893.appspot.com",
+  messagingSenderId: "472290903439",
+  appId: "1:472290903439:web:c2a0130149c1280ca7f7fa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
