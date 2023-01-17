2782b0c9b3660c38435620e4292fb2b72bc590eb
manuhutu344 <nandoperdos2@gmail.com>
2023-01-17 10:41:10 +0900

    Membuat upload gambar dan video ke firebase

--- src/Component/ContentPostContainer/ContentPost.js
+++ src/Component/ContentPostContainer/ContentPost.js
@@ -13,11 +13,12 @@ function ContentPost() {
   let user = userDetails.user
   console.log(user)
   let id = user.other._id
-  const [file, setfile] = useState('')
-  const [file2, setfile2] = useState('')
+  const [file, setfile] = useState(null)
+  const [file2, setfile2] = useState(null)
   console.log(file?.name)
   const handlePost = (e)=>{
     e.preventDefault()
+    if(file !== null){
     const fileName = new Date().getTime() + file?.name
     const storage = getStorage(app)
     const storageRef = ref(storage, fileName)
@@ -47,7 +48,38 @@ function ContentPost() {
       console.log('File available at', downloadURL);
     });
   }
+);}else if(file2 !== null){
+  const fileName = new Date().getTime() + file2?.name
+    const storage = getStorage(app)
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, file2);
+    uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+    }
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('Video File', downloadURL);
+    });
+  }
 );
+}
   }
   return (
     <div>
