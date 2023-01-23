842e1fc21371efe04b949e0a7d850709053771dc
manuhutu344 <nandoperdos2@gmail.com>
2023-01-23 10:17:54 +0900

    Membuat model ResetToken

---
+++ Backend/Models/ResetToken.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const ResetTokenSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    createAt:{
+        type: String,
+        required: true
+    },
+})
+ResetTokenSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10)
+    if(this.isModified('token')){
+        const hash = await bcrypt.hash(this.token, salt)
+        this.token = hash
+    }
+    next()
+})
+module.exports = mongoose.model('ResetToken', ResetTokenSchema)
