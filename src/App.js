61c01fdfb7de3f38fe449318386573718ddaf571
manuhutu344 <nandoperdos2@gmail.com>
2023-02-06 09:31:06 +0900

    Merubah userdetails mejadi userdetails? agar signup serta home serta login tidak errror

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Profile from './Pages/Profile/Profile';
 import {Routes, BrowserRouter, Route, Navigate} from 'react-router-dom'
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Register/Signup';
+import Verifyemail from './Pages/VerifyEmail/Verifyemail'
 import { useSelector } from 'react-redux';
 
 function App() {
   const userDetails = useSelector((state)=>state.user)
-  let user = userDetails.user
-  console.log(user)
+  let user = userDetails?.user
   return (
     <div className="App">
     <BrowserRouter>
     <Routes>
-    <Route path='/' element={<Home />}></Route>
+    <Route path='/' element={user?.other?.verifed === true ? <Home /> :  <Navigate to={"/login"} replace={true}/>}></Route>
     <Route path='/profil/:id' element={<Profile />}></Route>
-    <Route path='/login' element={ <Login />}></Route>
+    <Route path='/login' element={ user?.other?.verifed === true? <Navigate to={"/"} replace={true}/> : <Login />}></Route>
     <Route path='/signup' element={  <Signup />}></Route>
+    <Route path="/verify/email" element={user?.Status === 'Pending' ? <Verifyemail/> : user?.other?.verifed === true ? <Navigate to={"/"} replace={true}/> : <Login/>}></Route>
     </Routes>
     </BrowserRouter>
 {/* Memunculkan Home yang berisi leftbar, rightbar, dan mainpost */}
